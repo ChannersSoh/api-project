68c44a8035d287a02a834910c2cc57e03017548b
Charnjeet Sohal <charnjeetsohal@outlook.com>
2024-05-30 15:08:39 +0100

    This commit will add a topic query and refactor based on feedback

--- models/models.js
+++ models/models.js
@@ -4,8 +4,8 @@ exports.selectTopics = () => {
     return db.query(`SELECT * FROM topics`);
 };
 
-exports.selectArticles = (sort_by = "created_at", order = "DESC") => {
-    const validSortBy = ["created_at", "title", "author"];
+exports.selectArticles = (sort_by = "created_at", order = "DESC", topic) => {
+    const validSortBy = ["created_at", "title", "author", "topic", "votes"];
     const validOrder = ["ASC", "asc", "DESC", "desc"];
 
     if ((sort_by && !validSortBy.includes(sort_by)) ||
@@ -17,12 +17,18 @@ exports.selectArticles = (sort_by = "created_at", order = "DESC") => {
                          articles.votes, articles.article_img_url, 
                          COUNT(comments.comment_id)::INT AS comment_count
                          FROM articles
-                         LEFT JOIN comments ON articles.article_id = comments.article_id
-                         GROUP BY articles.article_id `;
+                         LEFT JOIN comments ON articles.article_id = comments.article_id `;
+        
+        let queryParams = [];
 
-        sqlQuery += ` ORDER BY ${sort_by} ${order}`;
+        if (topic){
+            sqlQuery += ` WHERE articles.topic = $1`;
+            queryParams.push(topic);
+        }
 
-        return db.query(sqlQuery).then(({rows}) => {
+        sqlQuery += ` GROUP BY articles.article_id ORDER BY ${sort_by} ${order}`;
+
+        return db.query(sqlQuery, queryParams).then(({rows}) => {
             return rows
         });
 };
@@ -82,11 +88,15 @@ exports.checkAuthorExists = (username) => {
         });
     };
 
-exports.updateArticles = (article_id, new_votes) => {
+exports.updateArticles = (article_id, inc_votes) => {
+    if(!inc_votes){
+        return Promise.reject({ status: 400, msg: 'Invalid Input' })
+    }
+
   return db.query(`UPDATE articles 
                    SET votes = votes + $1
                    WHERE article_id = $2
-                   Returning *`, [new_votes, article_id])
+                   Returning *`, [inc_votes, article_id])
         .then(({rows}) => {
             if(rows.length === 0) {
                 return Promise.reject({ status: 404, msg: 'Article does not exist' })
@@ -98,4 +108,9 @@ exports.updateArticles = (article_id, new_votes) => {
 exports.removeComment = (comment_id) => {
    return db.query(`DELETE FROM comments 
               WHERE comment_id = $1`, [comment_id])
+              .then((result) => {
+                if (result.rowCount === 0) {
+                    return Promise.reject({ status: 404, msg: 'Comment does not exist' });
+                }
+            });
 };
